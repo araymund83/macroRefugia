ecf1c08503c705c5742de39e75293387243fc08a
Ana Ray <araymund83@gmail.com>
2022-06-10 15:48:18 -0700

    feat-several group species added

--- 00_master.R
+++ 00_master.R
@@ -2,13 +2,13 @@ require(pacman)
 pacman::p_load('glue', 'googledrive', 'purrr', 'reproducible', 'tidyverse')
 
 
-#download boreal bird species
-# birdList <- c("ALFL", "AMRE", "ATTW", "BBWA", "BBWO", "BCCH", "BLBW", "BLPW",
-#              "BOCH", "BOOW", "BOWA", "CAJA", "CAWA", "CMWA", "CONW", "EVGR", 
-#              "FOSP", "GCKI", "GCSP", "GCTH", "GGOW", "HASP", "LEFL", "LISP", 
-#              "MAWA", "MOWA", "NHOW", "NOGO", "NOWA", "NSHR", "NSWO", "OSFL", 
-#              "PAWA", "PHVI", "PIGR", "PUFI", "RBNU", "RECR", "RUBL", "RUGR",
-#              "SPGR", "SWTH", "TEWA", "WTSP", "WWCR", "YBFL", "YEWA", "YRWA")
+#download boreal forest bird species
+birdList <- c("ALFL", "AMRE", "ATTW", "BBWA", "BBWO", "BCCH", "BLBW", "BLPW",
+             "BOCH", "BOOW", "BOWA", "CAJA", "CAWA", "CMWA", "CONW", "EVGR",
+             "FOSP", "GCKI", "GCSP", "GCTH", "GGOW", "HASP", "LEFL", "LISP",
+             "MAWA", "MOWA", "NHOW", "NOGO", "NOWA", "NSHR", "NSWO", "OSFL",
+             "PAWA", "PHVI", "PIGR", "PUFI", "RBNU", "RECR", "RUBL", "RUGR",
+             "SPGR", "SWTH", "TEWA", "WTSP", "WWCR", "YBFL", "YEWA", "YRWA")
 
 ##download western forest species 
 birdList <- c("ACWO", "AMDI", "BHGR", "BLSW", "BRCR", "BTAH", "BTPI", "BTYW",
@@ -20,6 +20,21 @@ birdList <- c("ACWO", "AMDI", "BHGR", "BLSW", "BRCR", "BTAH", "BTPI", "BTYW",
               "RNSA", "RUHU", "SPOW", "SPTO", "SSHA", "STJA", "TOSO", "TOWA", 
               "VASW", "VATH", "VGSW", "VIWA", "WEBL", "WESO", "WETA", "WEWP", 
               "WHWO", "WIFL", "WISA", "WIWA", "WOSJ", "YBMA", "ZTHA")
+
+## arctic species
+birdList <- c('AMPI', 'ARWA', 'ATSP', 'BCRF', 'BLRF', 'CORE', 'GCRF', 'GYRF',
+              'HORE', 'LALO', 'RLHA', 'ROPT', 'SMLO', 'SNBU', 'SNOW', 'WIPT')
+
+## aridlands species
+birdList <- c('ABTO', 'ALHU', 'ATFL', 'BCFL', 'BCHU', 'BCSP', 'BCVI', 'BESP',
+              'BETH', 'BEVI', 'BEWR', 'BROC', 'BRSP', 'BTNG', 'BTSP', 'CACW',
+              'CAGN', 'CAKI', 'CALT', 'CANT', 'CANW', 'CAQU', 'CASW', 'CATH',
+              'CHRA', 'CHUK', 'COGD', 'COHU', 'COPO', 'CRCA', 'CRTH', 'FEPO',
+              'GAQU', 'GIFL', 'GIWO', 'GRFL', 'GRRO', 'GRSG', 'GRVI', 'GTTO',
+              'HASH', 'INDO', 'LAGO', 'LASP', 'LBWO', 'LCTH', 'LENI', 'LUWA',
+              'NOBT', 'PABU', 'PHAI', 'PRFA', 'PYRR', 'RCSP', 'ROWR', 'RWSP',
+              'SABS', 'SATH', 'SCOR', 'SCQU', 'TRKI', 'VABU', 'VEFL', 'VERD',
+              'WREN', 'WTKI', 'WTSW', 'WWDO')
 
 
 ##download audobon models for boreal forest birds
